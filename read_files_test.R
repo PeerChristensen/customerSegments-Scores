fe62928236a7d03c78e610c10f1f13c7e50e521a
Peer Christensen (admin) <pech_adm@saxo.local>
2020-02-04 14:15:12 +0100

    test paths

--- read_files_test.R
+++ read_files_test.R
@@ -1,11 +1,10 @@
 
-library(readr)
 
 print("Lad os se om absolutte stier virker...")
 
 Sys.sleep(5)
 
-a <- readr::read_rds("D:/R/RScripts/customerSegments&Scores/credentials.rds")
+a <- readr::read_rds("D:/R/RScripts/customerSegments_Scores/credentials.rds")
 
 print(a)
 print("det virker!")
